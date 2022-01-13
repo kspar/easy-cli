1c874c9a003bb00f670db0984b9d463c0ee3f3af
mrka <merilins2de@gmail.com>
2022-01-13 23:37:19 +0200

    Parameters updated

--- sync_grades.py
+++ sync_grades.py
@@ -6,5 +6,6 @@ import util
 
 def sync_grades(course_id: str):
 
-    resp = requests.post(conf.EMS_BASE_URL + '/courses/' + course_id + '/moodlesync/grades', headers=util.get_token_header())
+    resp = requests.post(conf.EMS_BASE_URL + '/courses/' + course_id + '/moodle/grades', headers=util.get_token_header())
     print(resp.status_code)
+    print(resp.json())
