4fb3b4b8e9153aeaab198513753b618376627c97
mrka <merilins2de@gmail.com>
2022-01-13 23:36:50 +0200

    URL updated

--- link_moodle.py
+++ link_moodle.py
@@ -3,13 +3,17 @@ import requests
 import conf
 import util
 
-def link_moodle_course(course_id: str, moodle_short_name: str, sync_students: bool, sync_grades: bool):
+def link_moodle_course(course_id: str, moodle_short_name: str, sync_students: bool, sync_grades: bool, force: bool):
     body = {
-        "moodle_short_name": moodle_short_name,
-        "sync_students": sync_students,
-        "sync_grades": sync_grades
+        "moodle_props": {
+            "moodle_short_name": moodle_short_name,
+            "sync_students": sync_students,
+            "sync_grades": sync_grades
+        },
+        "force": force
     }
 
-    resp = requests.post(conf.EMS_BASE_URL + '/courses/' + course_id + '/moodle', json=body, headers=util.get_token_header())
+    resp = requests.put(conf.EMS_BASE_URL + '/courses/' + course_id + '/moodle', json=body, headers=util.get_token_header())
     print(resp.status_code)
+    print(resp.json())
 
